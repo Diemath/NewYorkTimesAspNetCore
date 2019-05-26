5e2484d67acf9639c5158f83c71b14881d2e8caf
Dmitriy Yegorov <mrdiemath@outlook.com>
2019-05-26 22:20:10 +0300

    Create empty project and add Nancy to it

---
+++ NancyApi/NancyApi/HomeModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+
+namespace NancyApi
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get("/", _ => "Hello World!!!");
+      Get("/about", _ => "Brought to you buy FancyFX");
+    }
+  }
+}
