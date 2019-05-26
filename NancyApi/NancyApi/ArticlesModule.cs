21c0716f77fa9f4cb8413eafa220900dce9fb06d
Dmitriy Yegorov <mrdiemath@outlook.com>
2019-05-27 00:34:16 +0300

    Use Nancy bind

---
+++ NancyApi/NancyApi/ArticlesModule.cs
@@ -0,0 +1,48 @@
+﻿using Nancy;
+using Nancy.ModelBinding;
+using System;
+
+namespace NancyApi
+{
+  public class ArticlesModule : NancyModule
+  {
+    public ArticlesModule()
+    {
+      Get("/", _ => {
+        return "Hello World!!!";
+      });
+
+      Get("/list/{section}", _ => {
+        var param = this.Bind<ArticleInputParams>();
+        return $"section: {param.Section}";
+      });
+
+      Get("/list/{section}/first", _ => {
+        var param = this.Bind<ArticleInputParams>();
+        return $"section: {param.Section}";
+      });
+
+      Get("/list/{section}/{updatedDate}", _ => {
+        var param = this.Bind<ArticleInputParams>();
+        return $"section: {param.Section}; updatedDate: {param.UpdatedDate}";
+      });
+
+      Get("/article/{shortUrl}", _ => {
+        var param = this.Bind<ArticleInputParams>();
+        return $"shortUrl: {param.ShortUrl}";
+      });
+
+      Get("/group/{section}", _ => {
+        var param = this.Bind<ArticleInputParams>();
+        return $"section: {param.Section}";
+      });
+    }
+  }
+
+  public class ArticleInputParams
+  {
+    public string Section { get; set; } 
+    public DateTime UpdatedDate { get; set; }
+    public string ShortUrl { get; set; }
+  }
+}
