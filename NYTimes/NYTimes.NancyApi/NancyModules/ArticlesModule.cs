390fbf2ccac5b761a24390d52f73e960b40d2c1b
Serhii Kudriashov <serhii.kudriashov@lebara.com>
2019-05-28 17:42:10 +0300

    no message

--- NYTimes/NYTimes.NancyApi/NancyModules/ArticlesModule.cs
+++ NYTimes/NYTimes.NancyApi/NancyModules/ArticlesModule.cs
@@ -13,147 +13,147 @@ using System.Linq;
 
 namespace NancyApi.NancyModules
 {
-  public class ArticlesModule : NancyModule
-  {
-    private readonly IArticleService _articleService;
-
-    private const string _validDateFormat = "yyyy-MM-dd";
-
-    public ArticlesModule(IArticleService articleService, INancyEnvironment environment)
+    public class ArticlesModule : NancyModule
     {
-      _articleService = articleService;
+        private readonly IArticleService _articleService;
 
-      Get("/", _ =>
-      {
-        return string.Empty;
-      });
+        private const string _validDateFormat = "yyyy-MM-dd";
 
-      Get("/list/{section}", async _ =>
-      {
-        var @params = this.Bind<ArticlesBySectionQueryParams>();
-        var validationResult = this.Validate(@params);
-
-        if (!validationResult.IsValid)
+        public ArticlesModule(IArticleService articleService, INancyEnvironment environment)
         {
-          return GetErrorResult(this, validationResult);
+            _articleService = articleService;
+
+            Get("/", _ =>
+            {
+                return string.Empty;
+            });
+
+            Get("/list/{section}", async _ =>
+            {
+                var @params = this.Bind<ArticlesBySectionQueryParams>();
+                var validationResult = this.Validate(@params);
+
+                if (!validationResult.IsValid)
+                {
+                    return GetErrorResult(this, validationResult);
+                }
+
+                var dtos = await _articleService.FilterArticlesAsync(
+            ParseSection(@params.Section)
+          );
+
+                return Response.AsJson(dtos.Select(MapToVm));
+            });
+
+            Get("/list/{section}/first", async _ =>
+            {
+                var @params = this.Bind<ArticlesBySectionQueryParams>();
+                var validationResult = this.Validate(@params);
+
+                if (!validationResult.IsValid)
+                {
+                    return GetErrorResult(this, validationResult);
+                }
+
+                var dto = await _articleService.GetArticleAsync(
+            ParseSection(@params.Section)
+          );
+
+                if (dto is null)
+                {
+                    return GetNotFoundResult(this);
+                }
+
+                return Response.AsJson(MapToVm(dto));
+            });
+
+            Get("/list/{section}/{updatedDate}", async _ =>
+            {
+                var @params = this.Bind<ArticlesBySectionAndDateQueryParams>();
+                var validationResult = this.Validate(@params);
+
+                if (!validationResult.IsValid)
+                {
+                    return GetErrorResult(this, validationResult);
+                }
+
+                var dtos = await _articleService.FilterArticlesAsync(
+            ParseSection(@params.Section),
+            ParseDate(@params.UpdatedDate)
+          );
+
+                return Response.AsJson(dtos.Select(MapToVm));
+            });
+
+            Get("/article/{shortUrl}", async _ =>
+            {
+                var @params = this.Bind<ArticlesByShortUrlQueryParams>();
+                var validationResult = this.Validate(@params);
+
+                if (!validationResult.IsValid)
+                {
+                    return GetErrorResult(this, validationResult);
+                }
+
+                var dto = await _articleService.GetArticleAsync(
+            @params.ShortUrl
+          );
+
+                if (dto is null)
+                {
+                    return GetNotFoundResult(this);
+                }
+
+                return Response.AsJson(MapToVm(dto));
+            });
+
+            Get("/group/{section}", async _ =>
+            {
+                var @params = this.Bind<ArticlesBySectionQueryParams>();
+                var validationResult = this.Validate(@params);
+
+                if (!validationResult.IsValid)
+                {
+                    return GetErrorResult(this, validationResult);
+                }
+
+                var dtos = await _articleService.GetGroupsAsync(
+            ParseSection(@params.Section)
+          );
+
+                return Response.AsJson(dtos.Select(MapToVm));
+            });
         }
 
-        var dtos = await _articleService.FilterArticlesAsync(
-          ParseSection(@params.Section)
-        );
-
-        return Response.AsJson(dtos.Select(MapToVm));
-      });
-
-      Get("/list/{section}/first", async _ =>
-      {
-        var @params = this.Bind<ArticlesBySectionQueryParams>();
-        var validationResult = this.Validate(@params);
-
-        if (!validationResult.IsValid)
+        private ArticleView MapToVm(ArticleDto dto)
         {
-          return GetErrorResult(this, validationResult);
+            return new ArticleView
+            {
+                Heading = dto.Title,
+                Updated = dto.UpdatedDateTime,
+                Link = dto.Url
+            };
         }
 
-        var dto = await _articleService.GetArticleAsync(
-          ParseSection(@params.Section)
-        );
-
-        if (dto is null)
+        private ArticleGroupByDateView MapToVm(ArticleGroupByDateDto dto)
         {
-          return GetNotFoundResult(this);
+            return new ArticleGroupByDateView
+            {
+                Total = dto.Total,
+                Date = dto.UpdatedDate.ToString(_validDateFormat)
+            };
         }
 
-        return Response.AsJson(MapToVm(dto));
-      });
+        private Response GetErrorResult(ArticlesModule articlesModule, ModelValidationResult modelValidationResult)
+          => articlesModule.Response.AsJson(modelValidationResult, HttpStatusCode.BadRequest);
 
-      Get("/list/{section}/{updatedDate}", async _ =>
-      {
-        var @params = this.Bind<ArticlesBySectionAndDateQueryParams>();
-        var validationResult = this.Validate(@params);
+        private Response GetNotFoundResult(ArticlesModule articlesModule)
+          => articlesModule.Response.AsJson(new { status = nameof(HttpStatusCode.NotFound) }, HttpStatusCode.NotFound);
 
-        if (!validationResult.IsValid)
-        {
-          return GetErrorResult(this, validationResult);
-        }
+        private Section ParseSection(string section)
+          => (Section)Enum.Parse(typeof(Section), section, true);
 
-        var dtos = await _articleService.FilterArticlesAsync(
-          ParseSection(@params.Section),
-          ParseDate(@params.UpdatedDate)
-        );
-
-        return Response.AsJson(dtos.Select(MapToVm));
-      });
-
-      Get("/article/{shortUrl}", async _ =>
-      {
-        var @params = this.Bind<ArticlesByShortUrlQueryParams>();
-        var validationResult = this.Validate(@params);
-
-        if (!validationResult.IsValid)
-        {
-          return GetErrorResult(this, validationResult);
-        }
-
-        var dto = await _articleService.GetArticleAsync(
-          @params.ShortUrl
-        );
-
-        if (dto is null)
-        {
-          return GetNotFoundResult(this);
-        }
-
-        return Response.AsJson(MapToVm(dto));
-      });
-
-      Get("/group/{section}", async _ =>
-      {
-        var @params = this.Bind<ArticlesBySectionQueryParams>();
-        var validationResult = this.Validate(@params);
-
-        if (!validationResult.IsValid)
-        {
-          return GetErrorResult(this, validationResult);
-        }
-
-        var dtos = await _articleService.GetGroupsAsync(
-          ParseSection(@params.Section)
-        );
-
-        return Response.AsJson(dtos.Select(MapToVm));
-      });
+        private DateTime ParseDate(string date)
+          => DateTime.ParseExact(date, _validDateFormat, CultureInfo.InvariantCulture);
     }
-
-    private ArticleView MapToVm(ArticleDto dto)
-    {
-      return new ArticleView
-      {
-        Heading = dto.Title,
-        Updated = dto.UpdatedDateTime,
-        Link = dto.Url
-      };
-    }
-
-    private ArticleGroupByDateView MapToVm(ArticleGroupByDateDto dto)
-    {
-      return new ArticleGroupByDateView
-      {
-        Total = dto.Total,
-        Date = dto.UpdatedDate.ToString(_validDateFormat)
-      };
-    }
-
-    private Response GetErrorResult(ArticlesModule articlesModule, ModelValidationResult modelValidationResult)
-      => articlesModule.Response.AsJson(modelValidationResult, HttpStatusCode.BadRequest);
-
-    private Response GetNotFoundResult(ArticlesModule articlesModule)
-      => articlesModule.Response.AsJson(new { status = nameof(HttpStatusCode.NotFound) }, HttpStatusCode.NotFound);
-
-    private Section ParseSection(string section)
-      => (Section)Enum.Parse(typeof(Section), section, true);
-
-    private DateTime ParseDate(string date)
-      => DateTime.ParseExact(date, _validDateFormat, CultureInfo.InvariantCulture);
-  }
 }
