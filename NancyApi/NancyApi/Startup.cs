3290da5f0cffd86b884296f69f6262eb131d1140
Dmitriy Yegorov <mrdiemath@outlook.com>
2019-05-28 07:52:46 +0300

    Fn

--- NancyApi/NancyApi/Startup.cs
+++ NancyApi/NancyApi/Startup.cs
@@ -9,19 +9,10 @@ namespace NancyApi
 {
   public class Startup
   {
-    public Startup(IConfiguration configuration)
-    {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     { 
-      var baseUrl = Configuration["NyTimesApi:BaseUrl"];
-      var uniqueIdentifier = Configuration["NyTimesApi:UniqueIdentifier"];
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
